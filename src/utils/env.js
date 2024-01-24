7e3c9065d4941ef1ca5f89843a7e35148e914a6f
jason <hainguyentran14@gmail.com>
2024-01-24 12:27:39 +0700

    fix url api

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,5 +1,5 @@
-// export const prod = "https://jellyfish-app-kafzn.ondigitalocean.app";
+export const prod = "https://jellyfish-app-kafzn.ondigitalocean.app";
 // https://lionfish-app-l56d2.ondigitalocean.app 
-export const prod = "http://localhost:8080";
+// export const prod = "http://localhost:8080";
 
 export const dev = "http://localhost:8080";
