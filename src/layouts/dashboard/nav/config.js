afce7b1e0a21c0fb6c60ddd55cad4af708b86f55
jason <hainguyentran14@gmail.com>
2024-01-29 10:12:45 +0700

    update nav

--- src/layouts/dashboard/nav/config.js
+++ src/layouts/dashboard/nav/config.js
@@ -12,60 +12,81 @@ const adminNav = r === "a";
 const managerNav = r === "m";
 const userNav = r === "u";
 
-if (adminNav) {
-  navConfig.push({
-    title: 'dashboard',
-    path: '/dashboard',
-    icon: icon('ic_analytics'),
-  });
-  navConfig.push({
-    title: 'network',
-    path: '/network',
-    icon: icon('ic_network'),
-  });
-  navConfig.push({
-    title: 'transaction',
-    path: '/transaction',
-    icon: icon('ic_transaction'),
-  });
-  navConfig.push({
-    title: 'commission',
-    path: '/commission',
-    icon: icon('ic_transaction'),
-  });
-}
-else if (managerNav) {
-  navConfig.push({
-    title: 'dashboard',
-    path: '/dashboard',
-    icon: icon('ic_analytics'),
-  });
-  navConfig.push({
-    title: 'network',
-    path: '/network',
-    icon: icon('ic_network'),
-  });
-  navConfig.push({
-    title: 'transaction',
-    path: '/transaction',
-    icon: icon('ic_transaction'),
-  });
-} else if (userNav) {
-  navConfig.push({
-    title: 'dashboard',
-    path: '/dashboard',
-    icon: icon('ic_analytics'),
-  });
-  navConfig.push({
-    title: 'network',
-    path: '/network',
-    icon: icon('ic_network'),
-  });
-  navConfig.push({
-    title: 'transaction',
-    path: '/transaction',
-    icon: icon('ic_transaction'),
-  });
-}
+// if (adminNav) {
+//   navConfig.push({
+//     title: 'dashboard',
+//     path: '/dashboard',
+//     icon: icon('ic_analytics'),
+//   });
+//   navConfig.push({
+//     title: 'network',
+//     path: '/network',
+//     icon: icon('ic_network'),
+//   });
+//   navConfig.push({
+//     title: 'transaction',
+//     path: '/transaction',
+//     icon: icon('ic_transaction'),
+//   });
+//   navConfig.push({
+//     title: 'commission',
+//     path: '/commission',
+//     icon: icon('ic_transaction'),
+//   });
+// }
+// else if (managerNav) {
+//   navConfig.push({
+//     title: 'dashboard',
+//     path: '/dashboard',
+//     icon: icon('ic_analytics'),
+//   });
+//   navConfig.push({
+//     title: 'network',
+//     path: '/network',
+//     icon: icon('ic_network'),
+//   });
+//   navConfig.push({
+//     title: 'transaction',
+//     path: '/transaction',
+//     icon: icon('ic_transaction'),
+//   });
+// } else if (userNav) {
+//   navConfig.push({
+//     title: 'dashboard',
+//     path: '/dashboard',
+//     icon: icon('ic_analytics'),
+//   });
+//   navConfig.push({
+//     title: 'network',
+//     path: '/network',
+//     icon: icon('ic_network'),
+//   });
+//   navConfig.push({
+//     title: 'transaction',
+//     path: '/transaction',
+//     icon: icon('ic_transaction'),
+//   });
+// }
+
+navConfig.push({
+  title: 'dashboard',
+  path: '/dashboard',
+  icon: icon('ic_analytics'),
+});
+navConfig.push({
+  title: 'network',
+  path: '/network',
+  icon: icon('ic_network'),
+});
+navConfig.push({
+  title: 'transaction',
+  path: '/transaction',
+  icon: icon('ic_transaction'),
+});
+navConfig.push({
+  title: 'commission',
+  path: '/commission',
+  icon: icon('ic_transaction'),
+});
 
 export default navConfig;
