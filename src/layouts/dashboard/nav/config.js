a8e678621c0ad2166e008ef0d74d75a49685d299
jason <hainguyentran14@gmail.com>
2024-02-02 17:46:18 +0700

    change role for nav

--- src/layouts/dashboard/nav/config.js
+++ src/layouts/dashboard/nav/config.js
@@ -9,47 +9,29 @@ const r = localStorage.getItem("r");
 
 const navConfig = [];
 const adminNav = r === "a";
+const superNav = r === "sa";
 const managerNav = r === "m";
-const userNav = r === "u";
 
-console.log(adminNav);
-console.log(managerNav);
-console.log(userNav);
-if (adminNav || managerNav) {
-  navConfig.push({
-    title: 'dashboard',
-    path: '/dashboard',
-    icon: icon('ic_analytics'),
-  });
-  navConfig.push({
-    title: 'network',
-    path: '/network',
-    icon: icon('ic_network'),
-  });
-  navConfig.push({
-    title: 'transaction',
-    path: '/transaction',
-    icon: icon('ic_transaction'),
-  });
+navConfig.push({
+  title: 'dashboard',
+  path: '/dashboard',
+  icon: icon('ic_analytics'),
+});
+navConfig.push({
+  title: 'network',
+  path: '/network',
+  icon: icon('ic_network'),
+});
+navConfig.push({
+  title: 'transaction',
+  path: '/transaction',
+  icon: icon('ic_transaction'),
+});
+
+if (superNav || adminNav || managerNav) {
   navConfig.push({
     title: 'commission',
     path: '/commission',
-    icon: icon('ic_transaction'),
-  });
-} else if (userNav) {
-  navConfig.push({
-    title: 'dashboard',
-    path: '/dashboard',
-    icon: icon('ic_analytics'),
-  });
-  navConfig.push({
-    title: 'network',
-    path: '/network',
-    icon: icon('ic_network'),
-  });
-  navConfig.push({
-    title: 'transaction',
-    path: '/transaction',
     icon: icon('ic_transaction'),
   });
 }
